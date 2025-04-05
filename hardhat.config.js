f2230dcb0f9e141603257189d1e0b9214f8570f4
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-04-06 01:15:32 +0530

    added the mcp-server.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.28",  // Update this to match your contract
   networks: {
-    hardhat: {},
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
